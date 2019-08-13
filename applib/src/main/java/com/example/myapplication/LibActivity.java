0fcacdc4322eecd87e98ea801bac8428c4301739
Tejashri Pawar <tejashri.pawar@JETSYNTHESYS.NET>
2019-08-13 19:56:27 +0530

    'updated'

--- applib/src/main/java/com/example/myapplication/LibActivity.java
+++ applib/src/main/java/com/example/myapplication/LibActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class LibActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class LibActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lib_main);
+        Toast.makeText(this, "ddd", Toast.LENGTH_SHORT).show();
     }
 }
